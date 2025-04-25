c6770362be5d1a57fc183670ec34a3ee93233c0b
RiccardoUnity <beninriccardo@gmail.com>
2025-04-25 19:27:57 +0200

    Hotfix
    - Aggiunto esercizio Bonus dato alla fine della lezione di teoria
    EPICODE

--- Assets/Script/EsercizioBonus.cs
+++ Assets/Script/EsercizioBonus.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class EsercizioBonus : MonoBehaviour
 {
-    //Far vedere la tabellina del 3, poi tutte le altre fino al 10
+    //Iterare tutte le tabelline fino al 10
 
     void Start()
     {
-        
+        // Devo partire da 1 e arrivare a 10, non posso usare come al solito i = 0; i < 10;
+        for (int i = 1; i < 11; i++)
+        {
+            for (int j = 1; j < 11; j++)
+            {
+                Debug.Log("Tabellina del " + i + " - " + j * i);
+            }
+        }
     }
 }
